b527902089e0e5404502efc2a1eaddcec65f791c
Kristina <kristinagrozdanova914@yahoo.com>
2021-03-01 00:59:49 +0100

    Changed something

--- src/Registration.java
+++ src/Registration.java
@@ -1,7 +1,7 @@
 public class Registration {
 
     public static void main(String[] args) {
-        System.out.println("test12 name");
+        System.out.println("test12== name");
         System.out.println("Krissstina");
         System.out.println("Gozdanoa");
     }
