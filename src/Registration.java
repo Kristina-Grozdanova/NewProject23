561090c07e9eb5a116eec5230fdfa517c5b43110
Kristina <kristinagrozdanova914@yahoo.com>
2021-03-01 00:55:32 +0100

    Changed name Krissstina

--- src/Registration.java
+++ src/Registration.java
@@ -2,7 +2,7 @@ public class Registration {
 
     public static void main(String[] args) {
         System.out.println("test name");
-        System.out.println("Kristina");
+        System.out.println("Krissstina");
         System.out.println("Gozdanova");
     }
 }
