7237946f5ebae3b3541bda31498e0dfd549c2812
Kristina <kristinagrozdanova914@yahoo.com>
2021-03-01 00:41:07 +0100

    Change Last Name

--- src/Registration.java
+++ src/Registration.java
@@ -3,7 +3,7 @@ public class Registration {
     public static void main(String[] args) {
         System.out.println("test name");
         System.out.println("Kristina");
-        System.out.println("GRozdanova");
+        System.out.println("Gozdanova");
     }
 }
 
